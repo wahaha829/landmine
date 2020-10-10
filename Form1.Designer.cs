0af0ac3a6bb813d0df0a404feada1cfdf6ddb8ec
wahaha829 <61814252+wahaha829@users.noreply.github.com>
2020-10-10 23:27:00 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace landmine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button1.Location = new System.Drawing.Point(14, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 45);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button2.Location = new System.Drawing.Point(14, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 45);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(398, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 292);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(10, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "花費時間";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(10, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "地雷數量";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(754, 473);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "踩地雷9*9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; //Click事件要匯入這個命名空間
+
+namespace landmine
+{
+    
+    public partial class Form1 : Form
+    {
+        public static ButtonL[,] buttons = new ButtonL[9, 9];  //宣告9*9的二維按鈕陣列
+        public static Random rnd = new Random(); // 亂數物件
+        public static int rank = 9;
+        public DateTime date1 ;
+        // 不設static會跳錯誤 CS0120需要有物件參考，才可使用非靜態欄位、方法或屬性
+        public static System.Timers.Timer t = new System.Timers.Timer(); 
+
+        public class ButtonL : Button //這個繼承自Button的類別，放到Form1外面會怪怪的???
+        {
+            public int islandmine = 0; //屬性:是否為地雷
+            public int landmine_around = 0;
+            public int flag = 0; // 是否被插旗
+            public int landmine_isfindout = 0;
+            public int row, col;
+      
+            public void setlandmine(int randnum) //依據輸入的亂數設定是否地雷
+            {
+                if (randnum == 1)
+                {
+                    islandmine = 1;
+                }
+
+            }
+            public void Button_right_down(object sender, MouseEventArgs e)
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                {
+                    if (this.flag == 0)
+                    {
+                        // 更換按鈕上的img為插旗圖案
+                        this.flag = 1;
+                        this.BackgroundImage = global::landmine.Properties.Resources.flag;
+                        this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+                    }
+                    else
+                    {
+                        this.flag = 0;
+                        this.BackgroundImage = null; // 拿掉插旗圖案
+                    }
+                    if (islandmine == 1 && flag == 1)
+                    {
+                        landmine_isfindout = 1;
+                    }
+                    else
+                    {
+                        landmine_isfindout = 0;
+                    }
+                    if (findout_landmines() == countlandmines() && countlandmines() == countflags())
+                    {
+                    //插旗數=找出正確定雷數=地雷數總和
+                        MessageBox.Show("Well done!");
+                    }
+                }
+
+            }
+            public void ButtonL_click(object sender, EventArgs e)
+            // 按下按鍵要執行的函式，參數不放(object sender, EventArgs e)會error
+            {
+                if (this.Enabled == true && this.flag == 0)//Button的內建屬性(是否可按)
+                // 若不加上面這一行判斷式，遞迴執行時會出錯，蠻怪的
+                {
+                    this.Enabled = false; //按鈕設為不可按
+                    if (islandmine == 1)
+                    {
+                        this.Text = "B";
+                        showbuttons();
+                        timer_stop(); //踩到地雷停止計時
+                        MessageBox.Show("Game Over!");
+                        
+                    }
+                    else if (landmine_around >= 1)
+                        this.Text = "" + landmine_around;
+                    else
+                    {
+                        this.Text = "" + landmine_around;
+                        click_around(row, col);
+                    }
+                }  
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createbuttons();
+            count_all_landmines_around();
+        }
+
+        private void createbuttons()
+        {
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    int rndnum = rnd.Next(1, rank); //產生1~9亂數
+                    buttons[i, j] = new ButtonL();
+                    buttons[i, j].row = i;
+                    buttons[i, j].col = j;
+                    buttons[i, j].BackColor = System.Drawing.Color.LightGray;
+                    buttons[i, j].TabStop = false; //把鍵盤焦點功能關閉，不然會有鍵盤藍色框
+                    buttons[i, j].Location = new Point(j * 30, i * 30); //座標x軸是j，y軸是i
+                    buttons[i, j].Size = new Size(30, 30);
+                    this.Controls.Add(buttons[i, j]);  //在Form1.Controls裡面新增按紐
+                    buttons[i, j].setlandmine(rndnum); //設定是否為地雷
+                    buttons[i, j].Click += new EventHandler(buttons[i, j].ButtonL_click);
+                    // 因為右鍵點button不能觸發Click或MouseClick，改用MouseDown
+                    buttons[i, j].MouseDown += new MouseEventHandler(buttons[i, j].Button_right_down);
+                    
+                }
+            }
+            this.date1 = DateTime.Now; //設定起始時間
+            timer_start(); //開始計時
+            label2.Text = "地雷數量 " + countlandmines();
+        }
+        private void resetbuttons()
+        {
+        // 重置按鈕狀態
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    int rndnum = rnd.Next(1, rank); //產生1~9亂數
+                    buttons[i, j].islandmine = 0;
+                    buttons[i, j].BackgroundImage = null;
+                    buttons[i, j].flag = 0;
+                    buttons[i, j].setlandmine(rndnum); //設定是否為地雷
+                    buttons[i, j].Enabled = true; //按鈕設為可按
+                    buttons[i, j].Text = "" ;
+                }
+
+            }
+            label2.Text = "地雷數量 " + countlandmines();
+            count_all_landmines_around();
+            this.date1 = DateTime.Now; //開始計時
+        }
+        private static void showbuttons()
+        {
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    buttons[i, j].Enabled = false; //按鈕設為不可按
+                    if (buttons[i, j].islandmine == 1)
+                        buttons[i, j].Text = "B";
+                    else
+                        buttons[i, j].Text = "" + buttons[i, j].landmine_around;
+                }
+
+            }
+        }
+        
+        public static void click_around(int row, int col)
+        //按下周圍按鈕
+        {
+            // ButtonL_click原本是按鈕事件觸發，想直接呼叫就裡面放兩個null參數
+            if (row >= 1 && col >= 1)
+                buttons[row - 1, col - 1].ButtonL_click(null,null);
+            if (row >= 1)
+                buttons[row - 1, col].ButtonL_click(null, null);
+            if (row >= 1 && col <= rank - 2)
+                buttons[row - 1, col + 1].ButtonL_click(null, null);
+            if (col >= 1)
+                buttons[row, col - 1].ButtonL_click(null, null);
+            if (col <= rank - 2)
+                buttons[row, col + 1].ButtonL_click(null, null);
+            if (row <= rank - 2 && col >= 1)
+                buttons[row + 1, col - 1].ButtonL_click(null, null);
+            if (row <= rank - 2)
+                buttons[row + 1, col].ButtonL_click(null, null);
+            if (row <= rank - 2 && col <= rank - 2)
+                buttons[row + 1, col + 1].ButtonL_click(null, null);
+            
+        }
+
+
+        public int count_landmine_around(int row, int col)
+        //計算第[i,j]個按鈕周圍的地雷數量
+        {
+            int landmine_around = 0;
+            if(row >= 1 && col >= 1)
+                landmine_around += buttons[row-1,col-1].islandmine;
+            if (row >= 1)
+                landmine_around += buttons[row-1, col].islandmine;
+            if (row >= 1 && col <= rank-2)
+                landmine_around += buttons[row-1, col+1].islandmine;
+            if (col >= 1)
+                landmine_around += buttons[row, col-1].islandmine;
+            if (col <= rank-2)
+                landmine_around += buttons[row, col+1].islandmine;
+            if (row <= rank-2 && col >= 1)
+                landmine_around += buttons[row+1, col-1].islandmine;
+            if (row <= rank-2)
+                landmine_around += buttons[row+1, col].islandmine;
+            if (row <= rank-2 && col <= rank-2)
+                landmine_around += buttons[row+1, col+1].islandmine;
+            //MessageBox.Show("["+row+","+col+"]"+"周圍地雷數量" + landmine_around);
+            return landmine_around;
+        }
+
+        private void count_all_landmines_around()
+        //計算所有按鈕周圍的地雷數量
+        {
+            for (int i = 0; i <= rank - 1; i++)
+            {
+                for (int j = 0; j <= rank - 1; j++)
+                {
+                    //count_landmine_around(i, j);
+                    buttons[i, j].landmine_around = count_landmine_around(i, j);
+                }
+            }
+        }
+        private static int countlandmines()
+        //地雷數量總和
+        {
+            int landmines_amount = 0;
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    if (buttons[i, j].islandmine == 1)
+                        landmines_amount++;
+                }
+            }
+            return landmines_amount;
+        }
+        private static int countflags()
+        //插旗數量總和
+        {
+            int flags_amount = 0;
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    if (buttons[i, j].flag == 1)
+                        flags_amount++;
+                }
+            }
+            return flags_amount;
+        }
+        private static int findout_landmines()
+        // 找出的正確地雷數量總和
+        {
+            int findout_amount = 0;
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    if (buttons[i, j].landmine_isfindout == 1)
+                        findout_amount++;
+                }
+            }
+            return findout_amount;
+        }
+        private static int count_buttons_enabled()
+        //按下按鈕總和
+        {
+            int buttons_enabled = 0;
+            for (int i = 0; i < rank; i++)
+            {
+                for (int j = 0; j < rank; j++)
+                {
+                    if (buttons[i, j].Enabled == true)
+                        buttons_enabled++;
+                }
+            }
+            return buttons_enabled;
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            resetbuttons();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showbuttons();
+        }
+
+
+
+        /* 計時器 & 執行緒委派
+         * System.Timers名稱空間下的Timer類，使用Elapsed事件另開一個執行緒。
+         * 定義一個System.Timers.Timer物件，然後繫結Elapsed事件，
+         * 通過Start()方法來啟動計時，通過Stop()方法或者Enable=false停止計時
+        */
+        private void timer_start()
+        {
+            t.Interval = 1000; //多久執行一次
+            t.Enabled = true; //是否執行System.Timers.Timer.Elapsed事件
+            t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
+            t.AutoReset = true; //每到指定时间Elapsed事件是触发一次(false)还是一直触发(預設)(true)
+            t.Start();
+        }
+
+        private void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            CreateControlsByTimer();
+            //this.label3.Text = " 當前時間：" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            //如果在Elapsed绑定的方法中直接直接更新label3的Text，會跳"跨執行緒作業無效"
+        }
+
+
+        private void CreateControlsByTimer()
+        {
+
+            //判斷這個Label3的物件是否在同一個執行緒上
+            if (this.InvokeRequired)
+            {
+                //當InvokeRequired為true時，表示在不同的執行緒上，所以進行委派(調用Invoke方法)
+                //移至Action定義，寫道 public delegate void Action();
+                Action invokeAction = new Action(CreateControlsByTimer); //遞迴??
+                this.Invoke(invokeAction);
+            }
+            else
+            {
+                //表示在同一個執行緒上了，所以可以正常的呼叫到這個Label物件
+                DateTime date2 = DateTime.Now;
+                TimeSpan timespan = date2 - date1;
+                label3.Text = "花費時間 " + timespan.Minutes + "分"+ timespan.Seconds +"秒"; 
+            }
+
+        }
+
+        private static void timer_stop()
+        {
+            t.Stop();
+        }
+
+    }
+}
